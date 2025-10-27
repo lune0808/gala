292c5a5ccfb74bf782b13495688f3b4c2dcdda2a
lune0808 <yagigami.goatgod@gmail.com>
2025-10-27 16:49:57 +0100

    create instance

--- src/main.c
+++ src/main.c
@@ -26,6 +26,31 @@ GLFWwindow *init_window_or_crash(int width, int height, const char *title)
 	return win;
 }
 
+VkInstance vulkan_instance_or_crash()
+{
+	VkApplicationInfo app_desc = {
+		.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+		.pApplicationName = "Gala",
+		.applicationVersion = VK_MAKE_VERSION(0, 0, 0),
+		.pEngineName = "No engine",
+		.engineVersion = VK_MAKE_VERSION(0, 0, 0),
+		.apiVersion = VK_API_VERSION_1_0,
+	};
+	VkInstanceCreateInfo inst_desc = {
+		.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+		.pApplicationInfo = &app_desc,
+		.enabledLayerCount = 0,
+	};
+	inst_desc.ppEnabledExtensionNames = glfwGetRequiredInstanceExtensions(&inst_desc.enabledExtensionCount);
+	VkInstance inst;
+	VkResult status = vkCreateInstance(&inst_desc, NULL, &inst);
+	if (status != VK_SUCCESS) {
+		fprintf(stderr, "vkCreateInstance\n");
+		exit(1);
+	}
+	return inst;
+}
+
 static const int WIDTH = 800;
 static const int HEIGHT = 600;
 
@@ -36,6 +61,8 @@ int main()
 	uint32_t n_ext;
 	vkEnumerateInstanceExtensionProperties(NULL, &n_ext, NULL);
 	printf("%" PRIu32 " extensions supported for Vulkan\n", n_ext);
+	VkInstance inst = vulkan_instance_or_crash();
+	(void) inst;
 	while (!glfwWindowShouldClose(win)) {
 		glfwPollEvents();
 	}
