5e9b812de37f4305ca59891cab8eab5a52625c54
lune0808 <yagigami.goatgod@gmail.com>
2025-11-08 12:58:31 +0100

    slightly tweak circular include dependency

--- inc/memory.h
+++ inc/memory.h
@@ -4,14 +4,12 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #include "gpu.h"
-#include "hwqueue.h"
-typedef struct lifetime lifetime; // recursive include
 
 
 u32 constrain_memory_type(context *ctx, u32 allowed,
 	VkMemoryPropertyFlags cons);
 
-typedef struct {
+typedef struct vulkan_buffer {
 	VkBuffer handle;
 	VkDeviceMemory mem;
 	VkDeviceSize size;
@@ -23,8 +21,9 @@ vulkan_buffer buffer_create(context *ctx,
 
 void *buffer_map(context *ctx, vulkan_buffer buf);
 void buffer_unmap(context *ctx, vulkan_buffer buf);
+struct lifetime; // circular dependency
 vulkan_buffer data_upload(context *ctx, VkDeviceSize size, const void *data,
-	lifetime *l, VkBufferUsageFlags usage);
+	struct lifetime *l, VkBufferUsageFlags usage);
 
 #endif /* GALA_MEMORY_H */
 
